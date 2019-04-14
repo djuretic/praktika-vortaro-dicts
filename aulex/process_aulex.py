79d3b2021c2a37503b5e8a5f782d6724d5378c45
Dušan Juretić <dusan.juretic@gmail.com>
2019-04-14 10:58:59 -0400

    Refactor

--- aulex/process_aulex.py
+++ aulex/process_aulex.py
@@ -1,49 +1,11 @@
 import html2text
 import os
-import sqlite3
-
-def process_row(row, cursor):
-    if ":" not in row:
-        return
-    esperanto, english = row.split(':')
-    print(repr(esperanto), repr(english))
-    cursor.execute(
-        "INSERT INTO aulex (eo, es) VALUES (?, ?)",
-        (esperanto.strip(), english.strip()))
-
-
-def create_db(output_db):
-    base_dir = os.path.dirname(__file__)
-    db_filename = os.path.join(base_dir, output_db)
-    try:
-        os.remove(db_filename)
-    except:
-        pass
-    conn = sqlite3.connect(db_filename)
-    c = conn.cursor()
-    c.execute("""
-        CREATE TABLE aulex (
-            id integer primary key,
-            eo text,
-            es text
-        )
-    """)
-    return conn
-
+from eo_dicts.parser.dictionary import DictionaryParser
 
 def main():
-    conn = create_db(os.path.join(os.path.dirname(__file__), '..', 'output', 'aulex.db'))
-    cursor = conn.cursor()
+    output_db = os.path.join(os.path.dirname(__file__), '..', 'output', 'aulex.db')
     with open('eo-es.htm') as f:
-        for n, row in enumerate(html2text.html2text(f.read()).splitlines()):
-            if n <= 2:
-                continue
-            process_row(row, cursor)
-    cursor.execute("CREATE INDEX index_eo_aulex ON aulex (eo)")
-    cursor.execute("CREATE INDEX index_es_aulex ON aulex (es)")
-    conn.commit()
-    cursor.close()
-
+        DictionaryParser.parse_string(html2text.html2text(f.read()), target_lang="es", output_db=output_db, output_table="aulex", header_lines=5)
 
 if __name__ == '__main__':
     main()
--- eo_dicts/parser/dictionary.py
+++ eo_dicts/parser/dictionary.py
@@ -1,46 +1,60 @@
 import os
 import sqlite3
 
-def create_db(output_db, output_table, target_lang):
-    base_dir = os.path.dirname(__file__)
-    db_filename = os.path.join(base_dir, output_db)
-    try:
-        os.remove(db_filename)
-    except:
-        pass
-    conn = sqlite3.connect(db_filename)
-    c = conn.cursor()
-    c.execute("""
-        CREATE TABLE {table} (
-            id integer primary key,
-            eo text,
-            {lang} text
-        )
-    """.format(table=output_table, lang=target_lang))
-    return conn
-
-def process_row(row, cursor, output_table, target_lang, separator=":"):
-    if separator not in row:
-        return
-    parts = row.split(separator, maxsplit=1)
-    parts = [s.strip() for s in parts]
-    print(repr(parts))
-    cursor.execute(
-        "INSERT INTO {table} (eo, {lang}) VALUES (?, ?)".format(table=output_table, lang=target_lang),
-        (parts[0], parts[1]))
-
-
 class DictionaryParser:
     @classmethod
     def parse_string(cls, input_string, target_lang, output_db, output_table, header_lines=0):
-        conn = create_db(output_db, output_table, target_lang)
+        parser = cls(target_lang, output_db, output_table, header_lines)
+        parser.parse(input_string.splitlines())
+
+    @classmethod
+    def parse_file(cls, input_filename, target_lang, output_db, output_table, header_lines=0):
+        parser = cls(target_lang, output_db, output_table, header_lines)
+        with open(input_filename) as f:
+            parser.parse(f.readlines())
+
+    def __init__(self, target_lang, output_db, output_table, header_lines):
+        self.target_lang = target_lang
+        self.output_db = output_db
+        self.output_table = output_table
+        self.header_lines = header_lines
+
+    def create_db(self):
+        db_filename = self.output_db
+        try:
+            os.remove(db_filename)
+        except:
+            pass
+        conn = sqlite3.connect(db_filename)
+        c = conn.cursor()
+        c.execute("""
+            CREATE TABLE {table} (
+                id integer primary key,
+                eo text,
+                {lang} text
+            )
+        """.format(table=self.output_table, lang=self.target_lang))
+        return conn
+
+    def parse(self, generator):
+        conn = self.create_db()
         cursor = conn.cursor()
 
-        for n, row in enumerate(input_string.splitlines()):
-            if n < header_lines:
+        for n, row in enumerate(generator):
+            if n < self.header_lines:
                 continue
-            process_row(row, cursor, output_table, target_lang)
-        cursor.execute("CREATE INDEX index_eo_espdic ON {table} (eo)".format(table=output_table))
-        cursor.execute("CREATE INDEX index_{lang}_espdic ON {table} ({lang})".format(table=output_table, lang=target_lang))
+            self.process_row(row, cursor)
+        cursor.execute("CREATE INDEX index_eo_espdic ON {table} (eo)".format(table=self.output_table))
+        cursor.execute("CREATE INDEX index_{lang}_espdic ON {table} ({lang})".format(table=self.output_table, lang=self.target_lang))
         conn.commit()
         cursor.close()
+
+    def process_row(self, row, cursor, separator=":"):
+        if separator not in row:
+            return
+        parts = row.split(separator, maxsplit=1)
+        parts = [s.strip() for s in parts]
+        print(repr(parts))
+        cursor.execute(
+            "INSERT INTO {table} (eo, {lang}) VALUES (?, ?)".format(table=self.output_table, lang=self.target_lang),
+            (parts[0], parts[1]))
