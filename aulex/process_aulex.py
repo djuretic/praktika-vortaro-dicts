ef38541363b75fe5409fe3109d99dbba63f18451
Dušan Juretić <dusan.juretic@gmail.com>
2019-04-14 11:03:23 -0400

    Remove markdown from html2text output

--- aulex/process_aulex.py
+++ aulex/process_aulex.py
@@ -5,7 +5,8 @@ from eo_dicts.parser.dictionary import DictionaryParser
 def main():
     output_db = os.path.join(os.path.dirname(__file__), '..', 'output', 'aulex.db')
     with open('eo-es.htm') as f:
-        DictionaryParser.parse_string(html2text.html2text(f.read()), target_lang="es", output_db=output_db, output_table="aulex", header_lines=5)
+        text = html2text.html2text(f.read()).replace('_', '')
+        DictionaryParser.parse_string(text, target_lang="es", output_db=output_db, output_table="aulex", header_lines=5)
 
 if __name__ == '__main__':
     main()
