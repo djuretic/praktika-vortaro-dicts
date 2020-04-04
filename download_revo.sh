9fc121c7e3e8c300476489b6840286daa01a8cab
Dušan Juretić <dusan.juretic@gmail.com>
2020-04-03 21:53:08 -0300

    Write revo version used

--- download_revo.sh
+++ download_revo.sh
@@ -1,2 +1,7 @@
 #/usr/bin/env bash
-wget -O revoxml.zip $(lynx -nonumbers -dump -listonly http://reta-vortaro.de/tgz/ | grep revoxml) && unzip revoxml.zip
+ZIP_URL=$(lynx -nonumbers -dump -listonly http://reta-vortaro.de/tgz/ | grep revoxml)
+VERSION=$(echo $ZIP_URL | tr -dc '0-9')
+wget -O revoxml.zip $ZIP_URL
+rm -rf revo/cfg revo/dtd revo/smb revo/stl revo/xml revo/xsl
+unzip revoxml.zip
+echo $VERSION > revo/VERSION
