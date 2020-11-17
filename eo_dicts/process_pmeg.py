d204d0a23fa5278bee84f55c274512f4fcf86099
Dušan Juretić <dusan.juretic@gmail.com>
2020-11-17 20:02:19 -0300

    Read category from PMEG index

--- eo_dicts/process_pmeg.py
+++ eo_dicts/process_pmeg.py
@@ -3,44 +3,64 @@ import glob
 import json
 from lxml import etree
 from typing import Dict, List, Optional, Tuple
-from pprint import pprint
+
+# category, text, link
+PmegLink = Tuple[Optional[str], str, str]
+
+
+def parse_index_entry(tag) -> Tuple[str, List[PmegLink]]:
+    headword: Optional[str] = None
+    links: List[PmegLink] = []
+
+    for child in tag:
+        if child.tag == "strong":
+            word: str = etree.tostring(child, method="text", encoding="utf-8").decode("utf-8")
+            headword = word.strip().lower()
+            # print(word)
+        elif child.tag == "ul":
+            ul_class = child.get('class')
+            if ul_class == "chefligoj":
+                for li in child.xpath(".//li"):
+                    text = etree.tostring(li, method="text", encoding="utf-8").decode("utf-8").strip()
+                    link = list(li.iter("a"))[0].get('href')
+                    # print(link, text)
+                    links.append((None, text, link))
+            elif ul_class == "subvortoj":
+                for li in child.xpath("./li"):
+                    assert li.get('class') == 'subvorto'
+                    category = li.text or etree.tostring(li[0], method="text", encoding="utf-8").decode("utf-8")
+                    category = category.strip()
+                    print(headword, len(list(li.iter('li'))))
+                    for inner_li in li.xpath('./ul/li'):
+                        text = etree.tostring(inner_li, method="text", encoding="utf-8").decode("utf-8").strip()
+                        link = list(inner_li.iter("a"))[0].get('href')
+                        links.append((category, text, link))
+            else:
+                raise
+
+        else:
+            raise
+
+    if headword is None:
+        raise
+
+    return (headword, links)
 
 
 def process() -> None:
     base_dir = os.path.join(os.path.dirname(__file__), '..', 'pmeg')
     parser = etree.HTMLParser()
     index_files = glob.glob(os.path.join(base_dir, '*.html'))
+    index_files.sort()
 
-    pmeg_entry: Dict[str, List[Tuple[str, str]]] = {}
+    pmeg_entry: Dict[str, List[PmegLink]] = {}
 
     for index_file in index_files:
         print(index_file)
         with open(index_file, 'rb') as f:
             tree = etree.parse(f, parser)
             for tag in tree.xpath("//li[@class='art'][./strong[@class='chefvorto']/i]"):
-                headword: Optional[str] = None
-                links: List[Tuple[str, str]] = []
-
-                for child in tag:
-                    if child.tag == "strong":
-                        word: str = etree.tostring(child, method="text", encoding="utf-8").decode("utf-8")
-                        headword = word.strip().lower()
-                        print(word)
-                    elif child.tag == "ul":
-                        ul_class = child.get('class')
-                        if ul_class == "chefligoj":
-
-                            for li in child.xpath(".//li"):
-                                text = etree.tostring(li, method="text", encoding="utf-8").decode("utf-8").strip()
-                                link = list(li.iter("a"))[0].get('href')
-                                print(link, text)
-                                links.append((link, text))
-
-                    else:
-                        raise
-
-                if headword is None:
-                    raise
+                headword, links = parse_index_entry(tag)
 
                 for single_headword in headword.split(","):
                     pmeg_entry[single_headword.strip()] = links
